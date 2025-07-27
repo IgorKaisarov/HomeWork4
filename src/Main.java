d90fdc9ced3afd5784d6ec28c10cf691e8b340e4
IgorKaisarov <Igo444@mail.ru>
2025-07-27 16:27:59 +0600

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,19 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Home Work 4");
+
+        int age = 25;
+        if (age >= 18) {
+            System.out.println(" Если возраст человека равнен " + age + ", то он совершеннолетний.");
+        }
+
+        int age1 = 15;
+        if (age1 < 18) {
+            System.out.println("Если возраст человека равен " + age1 + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+
 
 
 
